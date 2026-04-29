d000cead6357b765e3b0ca103f611175a408fc02
besoftwarephase_it <besoftwarephase@gmail.com>
2026-04-29 15:31:29 +0530

    Update ADMIN_TO to include additional email address

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ const FILE_CONFIG = {
 
 const EMAIL_CONFIG = {
   FROM_ADDRESS : "Uway Careers <contact@uway.in>",
-  ADMIN_TO     : "contact@uway.in",
+  ADMIN_TO     : ["contact@uway.in","ashabliend@gmail.com"],
   ADMIN_CC     : ["nawinmbliend@gmail.com"],
 };
 
